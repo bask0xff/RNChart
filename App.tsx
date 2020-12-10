77c990d34ba9377ebe81f69fdd1b2f3de81caf43
baskov.sergey <baskov.sergey@wildberries.ru>
2020-12-10 09:58:13 +0300

    add spline functions

--- App.tsx
+++ App.tsx
@@ -127,6 +127,202 @@ function roundFloat(x: number, exp: number){
     return "0." + zeros + xx;
 }
 
+//let svg=document.documentElement /*svg object*/
+let S = new Array<Path>(); /*splines*/
+let V = new Array<Path>(); /*vertices*/
+let C 	/*current object*/
+let x0,y0	/*svg offset*/
+
+/*saves elements as global variables*/
+function init()
+{
+    /*create splines*/
+    S.push(createPath("blue",1));
+    S.push(createPath("red",1));
+    S.push(createPath("green",1));
+    S.push(createPath("brown",1));
+
+    /*create control points*/
+    V.push(createKnot(60,60));
+    V.push(createKnot(220,300));
+    V.push(createKnot(420,300));
+    V.push(createKnot(700,240));
+
+    updateSplines();
+}
+
+/*creates and adds an SVG circle to represent knots*/
+function createKnot(x,y):Path
+{
+    var C = new Path(); //document.createElementNS("http://www.w3.org/2000/svg","circle")
+    C.setAttributeNS(null,"r",22)
+    C.setAttributeNS(null,"cx",x)
+    C.setAttributeNS(null,"cy",y)
+    C.setAttributeNS(null,"fill","gold")
+    C.setAttributeNS(null,"stroke","black")
+    C.setAttributeNS(null,"stroke-width","6")
+    C.setAttributeNS(null,"onmousedown","startMove(evt)")
+    //svg.appendChild(C)
+    return C
+
+}
+
+class Path {
+    static setAttributeNS(name: string, value: string) {
+
+    }
+
+    setAttributeNS(name, value: string, number: number) {
+
+    }
+
+    getAttributeNS(param, cx: string) {
+        return "";
+    }
+}
+
+/*creates and adds an SVG path without defining the nodes*/
+function createPath(color,width)
+{
+    width = (typeof width == 'undefined' ? "8" : width);
+    var P = Path; // document.createElementNS("http://www.w3.org/2000/svg","path")
+    P.setAttributeNS("fill","none")
+    P.setAttributeNS("stroke",color)
+    P.setAttributeNS("stroke-width",width)
+    //svg.appendChild(P)
+    return P
+}
+
+/*from http://www.w3.org/Graphics/SVG/IG/resources/svgprimer.html*/
+function startMove(evt)
+{
+    /*SVG positions are relative to the element but mouse
+      positions are relative to the window, get offset*/
+    //x0 = getOffset(svg).left;
+    //y0 = getOffset(svg).top;
+
+    C=evt.target
+    //svg.setAttribute("onmousemove","move(evt)")
+    //svg.setAttribute("onmouseup","drop()")
+}
+
+/*called on mouse move, updates dragged circle and recomputes splines*/
+function move(evt)
+{
+    let x = evt.clientX-x0;
+    let y = evt.clientY-y0;
+
+    /*move the current handle*/
+    C.setAttributeNS(null,"cx",x)
+    C.setAttributeNS(null,"cy",y)
+    updateSplines();
+}
+
+/*called on mouse up event*/
+function drop()
+{
+    //svg = document.getElementsByTagName('svg')[0];
+    //svg.setAttributeNS(null, "onmousemove",null)
+}
+
+/*computes spline control points*/
+function updateSplines()
+{
+    /*grab (x,y) coordinates of the control points*/
+    let x=new Array();
+    let y=new Array();
+    for (let i=0;i<4;i++)
+    {
+        /*use parseInt to convert string to int*/
+        x[i]=parseInt(V[i].getAttributeNS(null,"cx"))
+        y[i]=parseInt(V[i].getAttributeNS(null,"cy"))
+    }
+
+    /*computes control points p1 and p2 for x and y direction*/
+    let px = computeControlPoints(x);
+    let py = computeControlPoints(y);
+
+    /*updates path settings, the browser will draw the new spline*/
+    for (let i=0;i<3;i++)
+        S[i].setAttributeNS(null,"d",
+            path(x[i],y[i],px.p1[i],py.p1[i],px.p2[i],py.p2[i],x[i+1],y[i+1]));
+}
+
+/*creates formated path string for SVG cubic path element*/
+function path(x1,y1,px1,py1,px2,py2,x2,y2)
+{
+    return "M "+x1+" "+y1+" C "+px1+" "+py1+" "+px2+" "+py2+" "+x2+" "+y2;
+}
+
+/*computes control points given knots K, this is the brain of the operation*/
+function computeControlPoints(K)
+{
+    let p1=new Array();
+    let p2=new Array();
+    let n = K.length-1;
+
+    /*rhs vector*/
+    let a=new Array();
+    let b=new Array();
+    let c=new Array();
+    let r=new Array();
+
+    /*left most segment*/
+    a[0]=0;
+    b[0]=2;
+    c[0]=1;
+    r[0] = K[0]+2*K[1];
+
+    /*internal segments*/
+    for (let i = 1; i < n - 1; i++)
+    {
+        a[i]=1;
+        b[i]=4;
+        c[i]=1;
+        r[i] = 4 * K[i] + 2 * K[i+1];
+    }
+
+    /*right segment*/
+    a[n-1]=2;
+    b[n-1]=7;
+    c[n-1]=0;
+    r[n-1] = 8*K[n-1]+K[n];
+
+    /*solves Ax=b with the Thomas algorithm (from Wikipedia)*/
+    for (let i = 1; i < n; i++)
+    {
+        let m = a[i]/b[i-1];
+        b[i] = b[i] - m * c[i - 1];
+        r[i] = r[i] - m*r[i-1];
+    }
+
+    p1[n-1] = r[n-1]/b[n-1];
+    for (let i = n - 2; i >= 0; --i)
+        p1[i] = (r[i] - c[i] * p1[i+1]) / b[i];
+
+    /*we have p1, now compute p2*/
+    for (let i=0;i<n-1;i++)
+        p2[i]=2*K[i+1]-p1[i+1];
+
+    p2[n-1]=0.5*(K[n]+p1[n-1]);
+
+    return {p1:p1, p2:p2};
+}
+
+/*code from http://stackoverflow.com/questions/442404/dynamically-retrieve-html-element-x-y-position-with-javascript*/
+function getOffset( el )
+{
+    let _x = 0;
+    let _y = 0;
+    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+        _x += el.offsetLeft - el.scrollLeft;
+        _y += el.offsetTop - el.scrollTop;
+        el = el.offsetParent;
+    }
+    return { top: _y, left: _x };
+}
+
+
 export default class App extends Component<AppScreenState> {
     state = {
         chartId: 0,
@@ -137,6 +333,7 @@ export default class App extends Component<AppScreenState> {
     }
 
     drawChart() {
+        init();
         if (!this.state.canvasCtx) return;
         const ctx: CanvasRenderingContext2D = this.state.canvasCtx;
         if (ctx) {
